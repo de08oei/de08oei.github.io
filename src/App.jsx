678380120b579091a12518b2423f4ca14d28c5c4
Deanna Oei <deannaoei123@gmail.com>
2023-07-23 20:51:14 -0400

    create components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './components/header/Header'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <About />
+      <Experience />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
